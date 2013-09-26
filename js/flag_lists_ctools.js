2d403880c796a6dbf576e94c8a3ff8194f6883e1
Mr Snow <mr.snow@houseoflaudanum.com>
2013-09-26 16:09:45 +1000

    Rework Ctools modal styling.

--- js/flag_lists_ctools.js
+++ js/flag_lists_ctools.js
@@ -4,17 +4,9 @@
 Drupal.theme.prototype.FlagListsModal = function () {
   var html = '';
 
-  html += '<div id="ctools-modal" class="popups-box">';
-  html += '  <div class="ctools-modal-content ctools-sample-modal-content">';
-  html += '    <table cellpadding="0" cellspacing="0" id="ctools-face-table">';
-  html += '      <tr>';
-  html += '        <td class="popups-tl popups-border"></td>';
-  html += '        <td class="popups-t popups-border"></td>';
-  html += '        <td class="popups-tr popups-border"></td>';
-  html += '      </tr>';
-  html += '      <tr>';
-  html += '        <td class="popups-cl popups-border"></td>';
-  html += '        <td class="popups-c" valign="top">';
+  html += '<div id="ctools-modal" class="flag-lists-modal">';
+  html += '  <div class="ctools-modal-content flag-lists-modal">';
+
   html += '          <div class="popups-container">';
   html += '            <div class="modal-header popups-title">';
   html += '              <span id="modal-title" class="modal-title"></span>';
@@ -25,15 +17,7 @@ Drupal.theme.prototype.FlagListsModal = function () {
   html += '            <div class="popups-buttons"></div>'; //Maybe someday add the option for some specific buttons.
   html += '            <div class="popups-footer"></div>'; //Maybe someday add some footer.
   html += '          </div>';
-  html += '        </td>';
-  html += '        <td class="popups-cr popups-border"></td>';
-  html += '      </tr>';
-  html += '      <tr>';
-  html += '        <td class="popups-bl popups-border"></td>';
-  html += '        <td class="popups-b popups-border"></td>';
-  html += '        <td class="popups-br popups-border"></td>';
-  html += '      </tr>';
-  html += '    </table>';
+
   html += '  </div>';
   html += '</div>';
 
